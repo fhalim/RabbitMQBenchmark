5f414cf17a51a7faef7a781fcef1e4416d953456
Fawad Halim <fawad@fawad.net>
2013-03-16 12:56:27 -0500

    Initial import

---
+++ src/main.go
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: fawad
+ * Date: 3/16/13
+ * Time: 12:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello world!")
+}
