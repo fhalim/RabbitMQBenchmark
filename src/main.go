bd682a3cb6ea1019640cb6cc2e934fe5d09841ef
Fawad Halim <fawad@fawad.net>
2013-04-21 21:11:34 -0500

    Refactored out constants

--- src/main.go
+++ src/main.go
@@ -5,14 +5,21 @@ import ("fmt"
 	"time"
 )
 
+const (
+	exchangeName = "bmexch"
+	queueName    = "bmqueue"
+	url          = "amqp://guest:guest@localhost:5672/"
+	messageCount = 100000
+)
+
 func getChannel() (*amqp.Connection, *amqp.Channel) {
-	conn, _ := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, _ := amqp.Dial(url)
 	channel, _ := conn.Channel()
 	return conn, channel
 }
 func doPublish(channel *amqp.Channel, count int) {
 	for i := 0; i < count; i++ {
-		channel.Publish("bmexch", "", false, false, amqp.Publishing {
+		channel.Publish(exchangeName, "", false, false, amqp.Publishing {
 				Headers:         amqp.Table{},
 				ContentType:     "text/plain",
 				ContentEncoding: "",
@@ -24,13 +31,12 @@ func doPublish(channel *amqp.Channel, count int) {
 
 }
 func doSubscribe(channel *amqp.Channel, count int) (<-chan bool) {
-	del, _ := channel.Consume("bmqueue", "", true, false, false, false, nil)
+	del, _ := channel.Consume(queueName, "", true, false, false, false, nil)
 	completed := 0
 	outChannel := make(chan bool)
 	go func(c <-chan amqp.Delivery) {
 		for _ = range c {
-			//fmt.Print(".")
-			completed += 1
+			completed++
 			if completed == count {
 				outChannel <- true
 			}
@@ -44,17 +50,16 @@ func doBenchmark() {
 	conn2, channel2 := getChannel()
 	defer conn1.Close()
 	defer conn2.Close()
-	channel1.ExchangeDeclare("bmexch", "fanout", true, false, false, false, nil)
-	channel1.QueueDeclare("bmqueue", true, false, false, false, nil)
-	channel1.QueueBind("bmqueue", "", "bmexch", false, nil)
-	messageCount := 100000
+	channel1.ExchangeDeclare(exchangeName, "fanout", true, false, false, false, nil)
+	channel1.QueueDeclare(queueName, true, false, false, false, nil)
+	channel1.QueueBind(queueName, "", exchangeName, false, nil)
 	fmt.Println("Publishing", messageCount, "messages")
 	startTime := time.Now()
 	go doPublish(channel1, messageCount)
 	<-doSubscribe(channel2, messageCount)
 	endTime := time.Now()
 	timeElapsed := endTime.Sub(startTime)
-	fmt.Println("Published", messageCount, "messages in", time.Duration(timeElapsed), "at", (float64(messageCount)/timeElapsed.Seconds()))
+	fmt.Println("Published", messageCount, "messages in", time.Duration(timeElapsed), "at", (float64(messageCount)/timeElapsed.Seconds()), "records/sec")
 }
 func main() {
 	doBenchmark()
